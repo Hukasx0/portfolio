42e2c180e5908de89d244bae2bf94c46b2ac4413
Hubert Kasperek <hubertkasp13@gmail.com>
2024-02-15 13:26:20 +0100

    Replace card tilt with card gradient

--- src/components/AnimatedProjectCard.jsx
+++ src/components/AnimatedProjectCard.jsx
@@ -1,40 +1,14 @@
-import { motion } from 'framer-motion';
-import { Tilt } from 'react-tilt';
-import { ProjectCard } from './ProjectCard';
+import dynamic from 'next/dynamic';
 
-const fadeIn = (direction, type, delay, duration) => {
-    return {
-      hidden: {
-        x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
-        y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
-        opacity: 0,
-      },
-      show: {
-        x: 0,
-        y: 0,
-        opacity: 1,
-        transition: {
-          type: type,
-          delay: delay,
-          duration: duration,
-          ease: "easeOut",
-        },
-      },
-    };
-  };
+import { ProjectCard } from './ProjectCard';
+const BackgroundGradient = dynamic(() => import('./ui/background-gradient'), {
+  ssr: true
+});
 
 export const AnimatedProjectCard = ({ id, name, desc, techs, img, githubLink, currentlyWorkingOn, websitePreview}) => {
     return (
-        <motion.div variants={fadeIn("up", "spring", id * 0.5, 0.75)}>
-            <Tilt
-            options={{
-                max: 45,
-                scale: 1,
-                speed: 450,
-            }}
-            className="bg-secondary p-5 rounded-2xl sm:w-[360px] w-full h-full">
-                <ProjectCard  name={name} desc={desc} techs={techs} img={img} githubLink={githubLink} currentlyWorkingOn={currentlyWorkingOn} websitePreview={websitePreview} />
-            </Tilt>
-            </motion.div>
+      <BackgroundGradient className="p-5 bg-secondary rounded-2xl sm:w-[360px] w-full h-full sm:p-10">
+        <ProjectCard  name={name} desc={desc} techs={techs} img={img} githubLink={githubLink} currentlyWorkingOn={currentlyWorkingOn} websitePreview={websitePreview} />
+      </BackgroundGradient>
     );
 };
