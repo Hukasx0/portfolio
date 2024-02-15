a70ede6fb28a8f6f49912dd30cb6c0437c61bbfd
Hubert Kasperek <hubertkasp13@gmail.com>
2024-02-15 16:46:00 +0100

    Add hero section 3D hover effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,44 @@
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+
 const Hero = () => {
   return (
-    <section className="flex flex-col items-center border border-black/[0.2] dark:border-white/[0.2] p-3 sm:p-5 lg:p-20 relative">
+    <CardContainer className="border border-black/[0.2] dark:border-white/[0.2] p-3 sm:p-5 lg:p-20 relative">
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+    <CardBody className="flex flex-col items-center">
+    <CardItem
+          translateZ="40"
+        >
       <div className="text-xs sm:text-sm uppercase font-semibold mt-2 text-primary tracking-[4px] text-center">Fullstack Developer</div>
+      </CardItem>
+      <CardItem
+          translateZ="100"
+        >
         <h1 className="font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[50px] tiny-hero-text lg:leading-[98px] mt-2">Hi there!
           <span className="wave lg:text-[65px] sm:text-[65px] xs:text-[65px] text-[50px] lg:leading-[80px] pl-5">👋</span>
         </h1>
-        <div className="text-muted-foreground my-4 font-medium lg:text-[40px] sm:text-[30px] xs:text-[40px] text-[20px] lg:leading-[40px] tiny-hero-subtext">
-          <span className="font-semibold drop-shadow">My name is <span className="font-bold bg-gradient-to-r from-indigo-500 to-purple-500 inline-block text-transparent bg-clip-text py-1">Hubert Kasperek</span></span>
+        </CardItem>
+        <div className="text-muted-foreground my-4 font-medium lg:text-[40px] sm:text-[30px] xs:text-[40px] text-[20px] lg:leading-[40px] tiny-hero-subtext flex flex-row items-center gap-1 sm:gap-2 lg:gap-3">
+        <CardItem
+          translateZ="75"
+        >
+          <span className="font-semibold drop-shadow">My name is </span>
+          </CardItem>
+          <CardItem
+          translateZ="90"
+        >
+          <span className="font-bold drop-shadow bg-gradient-to-r from-indigo-500 to-purple-500 inline-block text-transparent bg-clip-text py-1">Hubert Kasperek</span>
+          </CardItem>
         </div>
+        <CardItem
+          translateZ="50"
+        >
         <p className="text-muted-foreground my-4 text-center font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]">I create fast, efficient and cheap to maintain backends, along with a clean, intuitive and easy to navigate frontends</p>
-    </section>
+        </CardItem>
+    </CardBody>
+    </CardContainer>
   )
 }
 
