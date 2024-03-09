50aaa61b0ba5ac2cf61120a9ad3135ce691cbac8
Hubert Kasperek <hubertkasp13@gmail.com>
2024-03-09 20:28:21 +0100

    Small fix

--- src/components/ui/card-hover-effect.jsx
+++ src/components/ui/card-hover-effect.jsx
@@ -30,7 +30,7 @@ export const CardHoverEffect = ({
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                className="absolute inset-0 h-full w-full bg-secondary block  rounded-3xl"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
@@ -61,7 +61,7 @@ export const Card = ({
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-card border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        "rounded-2xl h-full w-full p-4 overflow-hidden bg-card border border-border group-hover:border-slate-700 relative z-20",
         className
       )}
     >
