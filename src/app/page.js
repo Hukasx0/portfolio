8ab2940c8f4176a61668b4839edd017ec815a946
Hubert Kasperek <hubertkasp13@gmail.com>
2024-03-15 21:39:41 +0100

    Fix padding

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ import About from '@/components/About';
 export default function Home() {
   return (
     <main>
-      <div className='inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
+      <div className='inset-0 -z-10 h-full pb-10 w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
         <Header />
         <div className='hero-container'>
           <Hero />
