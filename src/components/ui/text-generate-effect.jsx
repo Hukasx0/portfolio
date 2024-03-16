489e7b0f35f6983d63e6311978877701499b5e85
Hubert Kasperek <hubertkasp13@gmail.com>
2024-03-16 17:37:34 +0100

    Text is only animated if visible

--- src/components/ui/text-generate-effect.jsx
+++ src/components/ui/text-generate-effect.jsx
@@ -1,26 +1,28 @@
 "use client";
 import { useEffect } from "react";
 import { motion, stagger, useAnimate } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { cn } from "@/lib/utils";
 
-export const TextGenerateEffect = ({
-  words,
-  className,
-}) => {
+export const TextGenerateEffect = ({ words, className }) => {
   const [scope, animate] = useAnimate();
+  const { ref, inView } = useInView({ threshold: 0.5 });
   let wordsArray = words.split(" ");
+  
   useEffect(() => {
-    animate(
-      "span",
-      {
-        opacity: 1,
-      },
-      {
-        duration: 2,
-        delay: stagger(0.2),
-      }
-    );
-  }, [scope.current]);
+    if (inView) {
+      animate(
+        "span",
+        {
+          opacity: 1,
+        },
+        {
+          duration: 2,
+          delay: stagger(0.2),
+        }
+      );
+    }
+  }, [inView, scope.current]);
 
   const renderWords = () => {
     return (
@@ -40,7 +42,7 @@ export const TextGenerateEffect = ({
   };
 
   return (
-    <div className={cn("font-bold", className)}>
+    <div className={cn("font-bold", className)} ref={ref}>
       <div className="mt-4">
         <div className=" text-muted-foreground mt-4 mb-0 pb-0 text-center font-medium lg:text-[24px] sm:text-[18px] xs:text-[10px] text-[12px] lg:leading-[30px] italic leading-snug tracking-wide">
           {renderWords()}
