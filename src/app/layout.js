65c5895887d21cb7c62083908d1d83cde5ae45db
Hubert Kasperek <hubertkasp13@gmail.com>
2024-03-18 17:32:40 +0100

    Enabled transition on change

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,6 @@ export default function RootLayout({ children }) {
             attribute="class"
             defaultTheme="system"
             enableSystem
-            disableTransitionOnChange
           >
           {children}
         </ThemeProvider>
