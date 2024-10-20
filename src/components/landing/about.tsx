f6c00f9ce0bad7cad2062908f64b275a0029600d
Hubert Kasperek <hubertkasp13@gmail.com>
2024-10-20 18:10:01 +0200

    Update "About me" description

--- src/components/landing/about.tsx
+++ src/components/landing/about.tsx
@@ -8,13 +8,13 @@ export default function About() {
                 <h2 className="text-3xl font-bold mb-4">A little about me</h2>
                 <div className="space-y-4">
                     <p className="text-muted-foreground">
-                        I mainly program in <Link className="text-primary hover:underline" target="_blank" href="https://nextjs.org/">Next.js</Link> using <Link className="text-primary hover:underline" target="_blank" href="https://create.t3.gg/">T3 Stack</Link>, although I also experiment with other technologies (as you can see from <Link className="text-primary hover:underline" target="_blank" href="https://github.com/Hukasx0">my GitHub account</Link>)
+                        As a <span className="text-primary">versatile developer</span>, I primarily focus on <span className="text-primary">Next.js</span> and the <span className="text-primary">T3 Stack</span> for web development, while also being proficient in <span className="text-primary">C# and .NET</span> for creating robust solutions across various platforms.
                     </p>
                     <p className="text-muted-foreground">
-                        Creator of <Link className="text-primary hover:underline" target="_blank" href="https://blitzyapp.com/">BlitzyApp.com</Link>, <Link className="text-primary hover:underline" target="_blank" href="https://github.com/Hukasx0/ai-companion">AI-Companion</Link> and <Link className="text-primary hover:underline" target="_blank" href="https://github.com/Hukasx0/character-factory">Character Factory</Link>
+                        I have successfully built <span className="text-primary">complex web applications</span> and <span className="text-primary">SaaS solutions</span>, including <Link className="text-primary hover:underline font-bold" target="_blank" href="https://blitzyapp.com/">BlitzyApp.com</Link>, <Link className="text-primary hover:underline font-bold" target="_blank" href="https://github.com/Hukasx0/ai-companion">AI-Companion</Link>, and <Link className="text-primary hover:underline font-bold" target="_blank" href="https://github.com/Hukasx0/character-factory">Character Factory</Link>.
                     </p>
                     <p className="text-muted-foreground">
-                        I have the appropriate skills to create a <span className="text-primary">Software as a Service (SaaS) application</span> or a <span className="text-primary">micro SaaS application myself (or in a team)</span> from start to finish, regardless of whether it is hosted on VPS or Serverless.
+                        In addition to my development work, I <span className="text-primary">mentored fellow classmates</span>, preparing them for <span className="text-primary">INF.03 and INF.04 exams</span> (Polish <span className="text-primary">national exams</span> for the title of programming technician, covering web development, databases, and software engineering) in web development, C# .NET, and algorithms, which showcases my <span className="text-primary">strong understanding of the material</span> and <span className="text-primary">effective communication skills</span>.
                     </p>
                 </div>
             </Balancer>
