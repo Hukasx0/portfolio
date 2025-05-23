a7fc16382e38e570f552924a1e56b79009e122a4
Hubert Kasperek <82332291+Hukasx0@users.noreply.github.com>
2025-05-23 20:32:52 +0200

    Small fix

--- src/components/landing/certificates.tsx
+++ src/components/landing/certificates.tsx
@@ -4,19 +4,6 @@ import Link from "next/link";
 
 const certificates = [
   {
-    name: "INF.03 Professional Qualification",
-    issuer: "Central Examination Board of Poland",
-    date: "Obtained in 2023",
-    description: [
-      "State-certified qualification for Programmer Technician",
-      "Validates proficiency in web development and database management",
-      "Covers front-end (HTML5, CSS3, JavaScript) and back-end (PHP) development",
-      "Includes practical database design and implementation tasks",
-      "Consists of both theoretical knowledge and practical skills assessment"
-    ],
-    link: ""
-  },
-  {
     name: "INF.04 Professional Qualification",
     issuer: "Central Examination Board of Poland",
     date: "Obtained in 2025",
@@ -25,6 +12,19 @@ const certificates = [
       "Focuses on application development using C#, .NET, TypeScript and Angular",
       "Covers development of console, desktop, web, and mobile applications",
       "Includes object-oriented programming and algorithm implementation",
+      "Consists of both theoretical knowledge and practical skills assessment"
+    ],
+    link: ""
+  },
+  {
+    name: "INF.03 Professional Qualification",
+    issuer: "Central Examination Board of Poland",
+    date: "Obtained in 2023",
+    description: [
+      "State-certified qualification for Programmer Technician",
+      "Validates proficiency in web development and database management",
+      "Covers front-end (HTML5, CSS3, JavaScript) and back-end (PHP) development",
+      "Includes practical database design and implementation tasks",
       "Consists of both theoretical knowledge and practical skills assessment"
     ],
     link: ""
@@ -37,7 +37,7 @@ export default function Certificates() {
       <CardContent className="p-6">
           <h2 className="text-3xl font-bold mb-6">Certificates</h2>
           <div className="flex flex-col space-y-8">
-            {certificates.reverse().map((cert, index) => (
+            {certificates.map((cert, index) => (
               <div key={index} className="flex w-full">
                 <div className="mr-4 mt-1 flex-shrink-0">
                   <div className="flex items-center justify-center w-8 h-8 rounded-full bg-secondary dark:bg-primary">
